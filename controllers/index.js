df76a1bc57b53110d281c17d0cc4fc787190d243
Nwsheats <nwsheats@gmail.com>
2022-12-05 23:11:50 -0600

    worked on getting the npm run start to work

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes')
+const homeRoute = require('./homeRoutes')
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes)
 
 module.exports = router;
